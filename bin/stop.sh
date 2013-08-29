4778dd8fd85edb661e4113acdb3a93c5592bbdb8
RouxRC <b.ooghe@gmail.com>
2013-08-29 20:39:51 +0200

    stronger stop script

--- bin/stop.sh
+++ bin/stop.sh
@@ -6,9 +6,14 @@ LOCK="/tmp/$BOTENV.lock"
 if [ -e $LOCK ]; then
   master=`cat $LOCK`
   process=`ps x -f | grep 'python gazouilleur/bot.py' | grep -v grep | grep "$master" | awk -F " " '{print $2}'`
-  if [ ! -z $process ]; then
+  if [ ! -z "$process" ]; then
     echo "Stopping the bot"
     kill $process
+    sleep 0.5
+    check=`ps x -f | grep "$process" | grep -v grep | grep "$master" | awk -F " " '{print $2}'`
+    if [ ! -z "$check" ]; then
+      kill -9 $check > /dev/null 2>&1
+    fi 
     rm -f $LOCK
     exit
   fi
