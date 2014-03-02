5fe26bfa5c8ede3e23bf490e03fbed668d675b46
RouxRC <b.ooghe@gmail.com>
2014-03-02 13:34:23 +0100

    optionnalize script to email logs archive

--- bin/daily_mail.sh
+++ bin/daily_mail.sh
@@ -3,19 +3,41 @@
 #
 # v0.1 : 2010-10-18 ; teymour for supybot
 # v0.2 : 2012-08-25 ; Roux for gazouilleur
+# v0.3 : 2014-03-02 ; Roux optionnalize
 #
-# Set below which <CHAN>'s log will be sent to <EMAILDEST>
+# USAGE: bin/daily_mail.sh [ <CHAN> [<STARTDATE> [<EMAIL>]]]
+# Set below which <DEFAULT_EMAIL> will be sent to when not set in option
 # To be set in a crontab 
 # 30 03 * * * bash /home/gazouilleur/gazouilleur2/bin/daily_mail.sh
 # @reboot     bash /home/gazouilleur/gazouilleur2/bin/gazouilleur start --nologs
 
-# Set the appropriate info below
-BOT="gazouilleur2"
-CHAN="#regardscitoyens"
-EMAILDEST="contact@regardscitoyens.org"
+BOTPATH=$(echo $0 | sed 's/[^\/]*$//')".."
+DEFAULT_EMAIL="example@example.com"
 
-LOGPATH=$(echo $0 | sed 's/[^\/]*$//')../log/${BOT}_${CHAN}.log
-TMPPATH="/tmp/$BOT-$CHAN.tmp"
+CHAN="#"$1
+if [ "$CHAN" == "#" ]; then
+  CHAN="#"$(grep "['\"]\s*:\s*{\s*$" $BOTPATH/gazouilleur/config.py | head -n 1 | sed "s/^\s*['\"]\([^'\"]\+\)['\"].*$/\1/")
+fi
+
+DATE=$2
+if [ -z "$DATE" ]; then
+  DATE=$(date -d 'yesterday' '+%Y-%m-%d')
+fi
+
+EMAILDEST=$3
+if [ -z "$EMAILDEST" ]; then
+  EMAILDEST=$DEFAULT_EMAIL
+fi
+
+DEBUG=false
+if [ ! -z "$4" ]; then
+  DEBUG=true
+fi
+
+BOT=$(grep ^BOTNAME $BOTPATH/gazouilleur/config.py | sed "s/^.*['\"]\([^'\"]\+\)['\"].*$/\1/")
+
+LOGPATH=$BOTPATH/log/${BOT}_${CHAN}.log
+TMPPATH="/tmp/${BOT}-$CHAN.tmp"
 if test -f "$LOGPATH.1"; then
   cat "$LOGPATH.1" "$LOGPATH" > "$TMPPATH"
   LOGPATH="$TMPPATH"
@@ -24,14 +46,16 @@ fi
 MAX_CHAR=150
 
 NBLINE=$(wc -l $LOGPATH | sed 's/ .*//')
-BEGINLINE=$(grep -n ^$(date -d 'yesterday' '+%Y-%m-%d') $LOGPATH | head -n 1 | sed 's/:.*//')
+BEGINLINE=$(grep -n "^$DATE" $LOGPATH | head -n 1 | sed 's/:.*//')
 TAILLINE=$(( $NBLINE - $BEGINLINE + 5))
 tail -n $TAILLINE $LOGPATH | sed 's/^/ /' | fold -w $MAX_CHAR -s | sed 's/^\([^ ]\)/                   \1/' > /tmp/email_log.txt
 echo "" >> /tmp/email_log.txt
 echo "--" >> /tmp/email_log.txt
 echo "Envoyé par $0 via la crontab de l'utilisateur gazouilleur" >> /tmp/email_log.txt
-#DEBUG
-#cat /tmp/email_log.txt
-cat /tmp/email_log.txt | iconv -c -f UTF-8 -t ISO-8859-1 | mail -s "[Regards Citoyens] IRC log $(date -d 'yesterday' '+%Y-%m-%d')" $EMAILDEST
+if $DEBUG; then
+  cat /tmp/email_log.txt
+else
+  cat /tmp/email_log.txt | iconv -c -f UTF-8 -t ISO-8859-1 | mail -s "[$BOT] IRC log $CHAN since $DATE" $EMAILDEST
+fi
 
 rm -f "$TMPPATH"
