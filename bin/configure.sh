264a1d4b706a05bfdb913fcd6bf9921e97310116
RouxRC <b.ooghe@gmail.com>
2014-08-24 19:59:49 +0200

    there is no need to templatize starter...

--- bin/configure.sh
+++ bin/configure.sh
@@ -2,8 +2,8 @@
 
 echo "Configure..."
 echo "------------"
-for file in gazouilleur/config.py bin/gazouilleur; do
-  if ! test -f "$file" || [ "$file" == "bin/gazouilleur" ]; then
+for file in gazouilleur/config.py; do
+  if ! test -f "$file"; then
     echo " preparing $file from example"
     sed "s|##GAZOUILLEURPATH##|"`pwd`"|" $file.example > $file || exit 1
   fi
