eef1b734ce51f4a0eb289b1bc7be0d6ca3bc7740
RouxRC <b.ooghe@gmail.com>
2014-11-21 19:39:20 +0100

    add script to get top rts for each twitter account of the bot

---
+++ bin/list_top_rts.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from gazouilleur import config
+from pymongo import MongoClient
+from pprint import pprint
+db = MongoClient(config.MONGODB['HOST'], config.MONGODB['PORT'])[config.MONGODB['DATABASE']]
+
+for chan in config.CHANNELS:
+    if "TWITTER" not in config.CHANNELS[chan]:
+        continue
+    account = config.CHANNELS[chan]["TWITTER"]["USER"]
+    tweets = []
+    for tweet in db["tweets"].find({"channel": "#%s" % chan.lower(), "user": account.lower()}, fields=["uniq_rt_hash", "message"]):
+        tweets.append({"text": tweet["message"], "rts": db["tweets"].find({"channel": "#%s" % chan.lower(), "uniq_rt_hash": tweet["uniq_rt_hash"]}).count()})
+    top = sorted(tweets, key=lambda k: -k['rts'])[:10]
+    print "------------"
+    print "TOP 10 RTS for @%s" % account
+    print "------------"
+    pprint(top)
+    print ""
