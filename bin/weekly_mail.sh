82440a5952d3104186838f9a726545c6c060a6d4
RouxRC <b.ooghe@gmail.com>
2015-07-12 16:11:14 +0200

    add script for weekly digested mail

--- bin/weekly_mail.sh
+++ bin/weekly_mail.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-# Send by email logs of an IRC chan
+# Send by email digest log of an IRC chan (removes the bot's messages)
 #
-# v0.1 : 2010-10-18 ; teymour for supybot
-# v0.2 : 2012-08-25 ; Roux for gazouilleur
-# v0.3 : 2014-03-02 ; Roux optionnalize
-#
-# USAGE: bin/daily_mail.sh [ <CHAN> [<STARTDATE> [<EMAIL>]]]
+# USAGE: bin/weekly_mail.sh [ <CHAN> [<STARTDATE> [<EMAIL>]]]
 # Set in gazouilleur/config.py which <DEFAULT_EMAIL> will be sent to when not set in option
 # Then set in a crontab:
-# 30 03 * * * bash /home/gazouilleur/gazouilleur2/bin/daily_mail.sh
-# @reboot     bash /home/gazouilleur/gazouilleur2/bin/gazouilleur start --nologs
+# 30 03 * * * bash /home/gazouilleur/gazouilleur2/bin/weekly_mail.sh
 
 cd "$(dirname $0)"/..
 BOTPATH=$(pwd)
@@ -23,7 +18,7 @@ fi
 
 DATE=$2
 if [ -z "$DATE" ]; then
-  DATE=$(date -d 'yesterday' '+%Y-%m-%d')
+  DATE=$(date -d 'last-week' '+%Y-%m-%d')
 fi
 
 EMAILDEST=$3
@@ -46,6 +41,11 @@ LOGPATH=$BOTPATH/log/${BOT}_${CHAN}.log
 TMPPATH="/tmp/${BOT}-$CHAN.tmp"
 if test -f "$LOGPATH.1"; then
   cat "$LOGPATH.1" "$LOGPATH" > "$TMPPATH"
+  if test -f "$LOGPATH.2"; then
+    cp "$TMPPATH" "$TMPPATH".tmp
+    cat "$LOGPATH.2" "$TMPPATH".tmp > "$TMPPATH"
+    rm "$TMPPATH".tmp
+  fi
   LOGPATH="$TMPPATH"
 fi
 
@@ -55,6 +55,10 @@ NBLINE=$(wc -l $LOGPATH | sed 's/ .*//')
 BEGINLINE=$(grep -n "^$DATE" $LOGPATH | head -n 1 | sed 's/:.*//')
 TAILLINE=$(( $NBLINE - $BEGINLINE + 5))
 tail -n $TAILLINE $LOGPATH |
+  grep -v "[0-9][0-9]:[0-9][0-9]:[0-9][0-9] ${BOT}: " |
+  grep -v "[0-9][0-9]:[0-9][0-9]:[0-9][0-9] \S\+: \[.* joined\]$" |
+  grep -v "[0-9][0-9]:[0-9][0-9]:[0-9][0-9] \S\+: \[.* left.*\]$" |
+  sed 's/\([0-9][0-9]:[0-9][0-9]:[0-9][0-9] \S\+\)!\S\+@\S\+:/\1:/' |
   sed 's/^/ /' |
   fold -w $MAX_CHAR -s |
   sed 's/^\([^ ]\)/                   \1/' > /tmp/email_log.txt
@@ -64,7 +68,7 @@ echo "Envoyé par $0 via la crontab de l'utilisateur gazouilleur" >> /tmp/email_
 if $DEBUG; then
   cat /tmp/email_log.txt
 else
-  cat /tmp/email_log.txt | iconv -c -f UTF-8 -t ISO-8859-1 | mail -s "[$BOT] IRC log $CHAN since $DATE" $EMAILDEST
+  cat /tmp/email_log.txt | iconv -c -f UTF-8 -t ISO-8859-1 | mail -s "[$BOT] IRC weekly summary for $CHAN since $DATE" $EMAILDEST
 fi
 
 rm -f "$TMPPATH"
