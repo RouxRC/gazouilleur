f442294f7348bf6434cefdb82168d672896d4b8c
RouxRC <b.ooghe@gmail.com>
2016-01-04 22:08:41 +0100

    display likes

--- gazouilleur/bot.py
+++ gazouilleur/bot.py
@@ -958,10 +958,11 @@ class IRCBot(NamesIRCClient):
         text, self.cache_urls = yield clean_redir_urls(text.replace('\n', ' '), self.cache_urls)
         date = datetime.fromtimestamp(time.mktime(time.strptime(tweet.get('created_at', ''), '%a %b %d %H:%M:%S +0000 %Y'))+60*60).strftime('%Y-%m-%d %H:%M:%S').encode('utf-8')
         source = " - %s" % clean_html(tweet['source']).encode('utf-8')
-        retweets = " - %s RTs" % tweet['retweet_count'] if 'retweet_count' in tweet and tweet['retweet_count'] else ""
+        extra = " - %s RTs" % tweet['retweet_count'] if 'retweet_count' in tweet and tweet['retweet_count'] else ""
+        extra += u" - %s ♥" % tweet['favorite_count'] if 'favorite_count' in tweet and tweet['favorite_count'] else ""
         if light:
-            returnD("%s: %s — https://twitter.com/%s/status/%s (%s%s)" % (name, text.encode('utf-8'), name, tweet['id_str'].encode('utf-8'), date, retweets.encode('utf-8')))
-        returnD("%s (%d followers): %s — https://twitter.com/%s/status/%s (%s%s%s)" % (name, user['followers_count'], text.encode('utf-8'), name, tweet['id_str'].encode('utf-8'), date, source, retweets.encode('utf-8')))
+            returnD("%s: %s — https://twitter.com/%s/status/%s (%s%s)" % (name, text.encode('utf-8'), name, tweet['id_str'].encode('utf-8'), date, extra.encode('utf-8')))
+        returnD("%s (%d followers): %s — https://twitter.com/%s/status/%s (%s%s%s)" % (name, user['followers_count'], text.encode('utf-8'), name, tweet['id_str'].encode('utf-8'), date, source, extra.encode('utf-8')))
 
     @inlineCallbacks
     def command_show(self, rest, channel=None, nick=None):
