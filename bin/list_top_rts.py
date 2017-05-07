c08e64942b1505aff09ceefba43187604211c6f5
RouxRC <b.ooghe@gmail.com>
2017-05-07 17:09:08 +0200

    fix script top rts

--- bin/list_top_rts.py
+++ bin/list_top_rts.py
@@ -11,13 +11,13 @@ for chan in config.CHANNELS:
         continue
     account = config.CHANNELS[chan]["TWITTER"]["USER"]
     tweets = []
-    for tweet in db["tweets"].find({"channel": "#%s" % chan.lower(), "user": account.lower()}, fields=["uniq_rt_hash", "message"]):
+    for tweet in db["tweets"].find({"channel": "#%s" % chan.lower(), "user": account.lower()}, projection=["uniq_rt_hash", "message"]):
         if tweet["message"].startswith("RT @"):
             continue
         tweets.append({"text": tweet["message"], "rts": db["tweets"].find({"channel": "#%s" % chan.lower(), "uniq_rt_hash": tweet["uniq_rt_hash"]}).count()})
-    top = sorted(tweets, key=lambda k: -k['rts'])[:10]
+    top = sorted(tweets, key=lambda k: -k['rts'])[:25]
     print "------------"
-    print "TOP 10 RTS for @%s" % account
+    print "TOP 25 RTS for @%s" % account
     print "------------"
     pprint(top)
     print ""
