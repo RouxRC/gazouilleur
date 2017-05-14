19db62b5e0639b7977b587395b85880ace8acbc7
RouxRC <b.ooghe@gmail.com>
2017-05-14 17:10:27 +0200

    let weekly log accessible online

--- bin/weekly_mail.sh
+++ bin/weekly_mail.sh
@@ -62,13 +62,23 @@ tail -n $TAILLINE $LOGPATH |
   sed 's/^/ /' |
   fold -w $MAX_CHAR -s |
   sed 's/^\([^ ]\)/                   \1/' > /tmp/email_log.txt
-echo "" >> /tmp/email_log.txt
-echo "--" >> /tmp/email_log.txt
-echo "Envoyé par $0 via la crontab de l'utilisateur gazouilleur" >> /tmp/email_log.txt
 if $DEBUG; then
   cat /tmp/email_log.txt
 else
-  cat /tmp/email_log.txt | iconv -c -f UTF-8 -t ISO-8859-1 | mail -s "[$BOT] IRC weekly summary for $CHAN since $DATE" $EMAILDEST
+  CLEANCHAN=$(echo $CHAN | sed 's/[^0-9a-z]//g')
+  URLGAZOU=$(grep "^URL_STATS" gazouilleur/config.py | head -1 | sed 's/^.*=\s*"//' | sed 's/\/\?".*$//')
+  HTMLFILE="weekly_irclog_$CLEANCHAN.html"
+  OUTPUT="web/$HTMLFILE"
+  echo "<html><body>" > $OUTPUT
+  cat /tmp/email_log.txt | sed -r 's/(https?:\S+)/<a href="\1">\1<\/a>/g' | sed 's/^ 20/<br>20/' >> $OUTPUT
+  echo "</body></html>" >> $OUTPUT
+  echo "$URLGAZOU/$HTMLFILE" > /tmp/email_log_full.txt
+  echo >> /tmp/email_log_full.txt
+  cat /tmp/email_log.txt >> /tmp/email_log_full.txt
+  echo >> /tmp/email_log_full.txt
+  echo "--" >> /tmp/email_log_full.txt
+  echo "Envoyé par $0 via la crontab de l'utilisateur gazouilleur" >> /tmp/email_log_full.txt
+  cat /tmp/email_log_full.txt | iconv -c -f UTF-8 -t ISO-8859-1 | mail -s "[$BOT] IRC weekly summary for $CHAN since $DATE" $EMAILDEST
 fi
 
 rm -f "$TMPPATH"
