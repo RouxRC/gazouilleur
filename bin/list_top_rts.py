aba6fcb7d23bbf85eefe4b8f1d04f74a89bca114
RouxRC <b.ooghe@gmail.com>
2018-03-12 00:34:27 +0100

    dislay more info

--- bin/list_top_rts.py
+++ bin/list_top_rts.py
@@ -11,10 +11,10 @@ for chan in config.CHANNELS:
         continue
     account = config.CHANNELS[chan]["TWITTER"]["USER"]
     tweets = []
-    for tweet in db["tweets"].find({"channel": "#%s" % chan.lower(), "user": account.lower()}, projection=["uniq_rt_hash", "message"]):
+    for tweet in db["tweets"].find({"channel": "#%s" % chan.lower(), "user": account.lower()}, projection=["uniq_rt_hash", "message", "link"]):
         if tweet["message"].startswith("RT @"):
             continue
-        tweets.append({"text": tweet["message"], "rts": db["tweets"].find({"channel": "#%s" % chan.lower(), "uniq_rt_hash": tweet["uniq_rt_hash"]}).count()})
+        tweets.append({"text": tweet["message"], "rts": db["tweets"].find({"channel": "#%s" % chan.lower(), "uniq_rt_hash": tweet["uniq_rt_hash"]}).count(), "link": tweet["link"]})
     top = sorted(tweets, key=lambda k: -k['rts'])[:25]
     print "------------"
     print "TOP 25 RTS for @%s" % account
